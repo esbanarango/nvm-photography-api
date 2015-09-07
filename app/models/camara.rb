2eb957c13e21832c5829cc2fd5d44c3e18fe01de
Esteban Arango Medina <marranoparael31@gmail.com>
2015-09-06 21:13:53 -0500

    Annotattion

--- app/models/camara.rb
+++ app/models/camara.rb
@@ -1,2 +1,12 @@
+# == Schema Information
+#
+# Table name: camaras
+#
+#  id         :integer          not null, primary key
+#  model      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Camara < ActiveRecord::Base
 end
--- app/models/session.rb
+++ app/models/session.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: sessions
+#
+#  id                   :integer          not null, primary key
+#  user_id              :integer
+#  authentication_token :string
+#  expires_at           :datetime
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#
+# Indexes
+#
+#  index_sessions_on_user_id  (user_id)
+#
+
 class Session < ActiveRecord::Base
 
   # Relations
